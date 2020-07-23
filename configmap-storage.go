9ad77b9d4c9a7974458b69ab150c7978220ff330
KJ <info.j.landwoner.k@gmail.com>
2020-07-24 07:49:40 +0900

    Fix initial commit

--- configmap-storage.go
+++ configmap-storage.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/informers"
@@ -21,22 +22,22 @@ const (
 	namePrefix = "storage.k8s.jlandowner.com"
 )
 
-// ConfigMapStorageClient is a manager of configmaps
-type ConfigMapStorageClient struct {
+// ConfigMapStorageManager is a manager of configmaps
+type ConfigMapStorageManager struct {
 	k8sclient *kubernetes.Clientset
 	LocalMaps map[string]*MapStorage
 	namespace string
 	lock      *sync.RWMutex
 }
 
-// MapStorage is a configmap as table
+// MapStorage is a configmap as m
 type MapStorage struct {
 	configMap *corev1.ConfigMap
 	lock      *sync.RWMutex
 }
 
-// NewConfigMapStorageClient returns ConfigMapStorageClient
-func NewConfigMapStorageClient(stopCh <-chan struct{}, namespace string) (*ConfigMapStorageClient, error) {
+// NewConfigMapStorageManager returns ConfigMapStorageManager
+func NewConfigMapStorageManager(stopCh <-chan struct{}, namespace string) (*ConfigMapStorageManager, error) {
 	cfg, err := rest.InClusterConfig()
 	if err != nil {
 		return nil, err
@@ -47,19 +48,18 @@ func NewConfigMapStorageClient(stopCh <-chan struct{}, namespace string) (*Confi
 		return nil, err
 	}
 
-	tables := make(map[string]*MapStorage, 0)
+	maps := make(map[string]*MapStorage, 0)
 
 	factory := informers.NewSharedInformerFactory(client, time.Minute)
 	listener := factory.Core().V1().ConfigMaps().Lister()
-	labelSelector := labels.NewSelector()
-	labelSelector, _ = labels.Parse(namePrefix + "/type in table")
+	labelSelector := getLabelSelector()
 
 	resync := func() {
 		ret, err := listener.List(labelSelector)
 		if err != nil {
 			return
 		}
-		syncLocalMap(tables, ret)
+		syncLocalMap(maps, ret)
 	}
 
 	informer := factory.Core().V1().ConfigMaps().Informer()
@@ -80,16 +80,16 @@ func NewConfigMapStorageClient(stopCh <-chan struct{}, namespace string) (*Confi
 		log.Println("informer stopped")
 	}()
 
-	return &ConfigMapStorageClient{
+	return &ConfigMapStorageManager{
 		k8sclient: client,
-		LocalMaps: tables,
+		LocalMaps: maps,
 		lock:      new(sync.RWMutex),
 		namespace: namespace,
 	}, nil
 }
 
-// CreateNewMapStorage creates new configmap as managed table
-func (c *ConfigMapStorageClient) CreateNewMapStorage(ctx context.Context, name string) (*MapStorage, error) {
+// CreateNewMapStorage creates new configmap as managed map
+func (c *ConfigMapStorageManager) CreateNewMapStorage(ctx context.Context, name string) (*MapStorage, error) {
 	_, exist := c.LocalMaps[name]
 	if exist {
 		return nil, fmt.Errorf("ConfigMap %s has already exist in cluster", name)
@@ -101,28 +101,32 @@ func (c *ConfigMapStorageClient) CreateNewMapStorage(ctx context.Context, name s
 	cm := &corev1.ConfigMap{}
 	cm.SetName(namePrefix + "." + name)
 	cm.SetLabels(getLabels())
+	cm.Data = map[string]string{"dummy": time.Now().String()}
 
 	res, err := c.k8sclient.CoreV1().ConfigMaps(c.namespace).Create(ctx, cm, metav1.CreateOptions{})
+	if apierrs.IsAlreadyExists(err) {
+		return c.GetMapStorage(name)
+	}
 	if err != nil {
 		return nil, err
 	}
 
-	table := &MapStorage{res, new(sync.RWMutex)}
-	c.LocalMaps[name] = table
+	m := &MapStorage{configMap: res, lock: new(sync.RWMutex)}
+	c.LocalMaps[name] = m
 
-	return table, nil
+	return m, nil
 }
 
-// DeleteMapStorage removes configmaptable
-func (c *ConfigMapStorageClient) DeleteMapStorage(ctx context.Context, name string) error {
-	table, exist := c.LocalMaps[name]
+// DeleteMapStorage removes configmap
+func (c *ConfigMapStorageManager) DeleteMapStorage(ctx context.Context, name string) error {
+	m, exist := c.LocalMaps[name]
 	if !exist {
 		return fmt.Errorf("MapStorage %s do not exist in cluster", name)
 	}
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	err := c.k8sclient.CoreV1().ConfigMaps(table.configMap.Namespace).Delete(ctx, table.configMap.Name, metav1.DeleteOptions{})
+	err := c.k8sclient.CoreV1().ConfigMaps(m.configMap.Namespace).Delete(ctx, m.configMap.Name, metav1.DeleteOptions{})
 	if err != nil {
 		return err
 	}
@@ -132,53 +136,72 @@ func (c *ConfigMapStorageClient) DeleteMapStorage(ctx context.Context, name stri
 }
 
 // GetMapStorage returns value by given key
-func (c *ConfigMapStorageClient) GetMapStorage(name string) (*MapStorage, error) {
-	table, exist := c.LocalMaps[name]
+func (c *ConfigMapStorageManager) GetMapStorage(name string) (*MapStorage, error) {
+	m, exist := c.LocalMaps[name]
 	if !exist {
 		return nil, fmt.Errorf("MapStorage %s do not exist in cluster", name)
 	}
-	return table, nil
+	return m, nil
+}
+
+// Commit commit the local change
+func (c *ConfigMapStorageManager) Commit(ctx context.Context, m *MapStorage) error {
+	ret, err := c.k8sclient.CoreV1().ConfigMaps(m.configMap.Namespace).Update(ctx, m.configMap, metav1.UpdateOptions{})
+	syncLocalMap(c.LocalMaps, []*corev1.ConfigMap{ret})
+	return err
 }
 
 // Upsert update or insert value by given key
-func (t *MapStorage) Upsert(key, value string) {
-	t.lock.Lock()
-	defer t.lock.Unlock()
+func (m *MapStorage) Upsert(ctx context.Context, key, value string) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
 
-	t.configMap.Data[key] = value
+	if m.configMap.Data != nil {
+		m.configMap.Data[key] = value
+	} else {
+		m.configMap.Data = map[string]string{key: value}
+	}
 }
 
 // Delete remove given key
-func (t *MapStorage) Delete(key string) {
-	t.lock.Lock()
-	defer t.lock.Unlock()
+func (m *MapStorage) Delete(ctx context.Context, key string) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
 
-	delete(t.configMap.Data, key)
+	delete(m.configMap.Data, key)
 }
 
 // Get returns value by given key
-func (t *MapStorage) Get(key string) (string, bool) {
-	val, ok := t.configMap.Data[key]
+func (m *MapStorage) Get(key string) (string, bool) {
+	val, ok := m.configMap.Data[key]
 	return val, ok
 }
 
 func syncLocalMap(localMap map[string]*MapStorage, syncList []*corev1.ConfigMap) {
 	for _, cm := range syncList {
 		namekey := extractBaseName(cm.Name)
-		table, exist := localMap[namekey]
+		m, exist := localMap[namekey]
 		if exist {
-			table.lock.Lock()
-			localMap[namekey] = &MapStorage{cm, new(sync.RWMutex)}
-			table.lock.Unlock()
+			m.lock.Lock()
+			m.configMap = cm
+			m.lock.Unlock()
 		} else {
-			localMap[namekey] = &MapStorage{cm, new(sync.RWMutex)}
+			localMap[namekey] = &MapStorage{configMap: cm, lock: new(sync.RWMutex)}
 		}
 	}
 }
 
+func getLabelSelector() labels.Selector {
+	labelSelector, err := labels.Parse(namePrefix + "/type in m")
+	if err != nil {
+		return labels.NewSelector()
+	}
+	return labelSelector
+}
+
 func getLabels() map[string]string {
 	labels := make(map[string]string, 0)
-	labels[namePrefix+"/type"] = "table"
+	labels[namePrefix+"/type"] = "m"
 	labels[namePrefix+"/locked"] = "false"
 	return labels
 }
